492a5bca427561050c5f041e966e6f29131dd786
ceboola <mrcarrr@gmail.com>
2017-07-18 13:18:41 +0200

    initial commit

---
+++ src/data/posts.js
@@ -0,0 +1,34 @@
+export const posts = [
+   {
+      "code":"BAcyDyQwcXX",
+      "caption":"Paździoch sprzedaje skarpety na pigalaku",
+      "likes":56,
+      "unlikes":0,
+      "id":"1161022966406956503",
+      "display_src":"https://d.wattpad.com/story_parts/172604056/images/1409f5d123799f93.jpg"
+   },
+   {
+      "code":"BAcJeJrQca9",
+      "caption":"Boczek nie taki tłusty",
+      "likes":59,
+      "unlikes":0,
+      "id":"1160844458347054781",
+      "display_src":"http://www.blasty.pl/upload/images/large/2017/01/swiat-wg-kiepskich_2017-01-23_00-34-50.jpg"
+   },
+   {
+      "code":"BAF_KY4wcRY",
+      "caption":"Gdzie są pierogies?",
+      "likes":49,
+      "unlikes":0,
+      "id":"1154606670337393752",
+      "display_src":"https://i.ytimg.com/vi/5p-4-GiDwMk/hqdefault.jpg"
+   },
+   {
+      "code":"BAPIPRjQce9",
+      "caption":"Panie zajęte jest!",
+      "likes":47,
+      "unlikes":0,
+      "id":"1157179863266871229",
+      "display_src":"http://www.repostuj.pl/upload/2017/01/original_137922_985f792b6d829202f61d4542af0e52ec.jpg"
+   }
+];
