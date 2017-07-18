3bb3f56e3205f9ed5dd59b72336940afa9f3aefb
ceboola <mrcarrr@gmail.com>
2017-07-18 13:18:00 +0200

    initial commit

---
+++ src/data/comments.js
@@ -0,0 +1,31 @@
+export const comments = {
+  "BAcyDyQwcXX":[
+    {
+      "text":"To panu zawsze się chce, kiedy mnie się chce!",
+      "user": "Marian",
+      "date": "13:37:00"
+    },
+    {
+      "text":"Marian Paździoch to jest ch...!",
+      "user": "Ferdek",
+      "date": "13:37:00"
+    },
+    {
+      "text":"Prostata! Precz z komuną! Niech żyje Koziołek Matołek!",
+      "user": "Babka",
+      "date": "13:37:00"
+    },
+    {
+      "text":"Ja se idę do siebie i se usmażę kiszeczki na tłuszczyku, tłuściutko tak jak lubię. 😍",
+      "user": "Boczek",
+      "date": "13:37:00"
+    }
+  ],
+  "BAPIPRjQce9":[
+    {
+      "text":"Całe twoje życie zaczyna się i kończy pod kiblem!",
+      "user": "Halina",
+      "date": "13:37:00"
+    }
+  ]
+};
