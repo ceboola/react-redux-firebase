7d22f4271b7536fbbab224ca7e334f9c87b44051
ceboola <mrcarrr@gmail.com>
2017-07-18 13:10:26 +0200

    initial commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {Single} from './Single';
+export {PhotoGrid} from './PhotoGrid';
+export {MainRender} from './MainRender';
+export {Photo, PhotoConnect} from './Photo';
+export {Comments} from './Comments';
+export {SignUp} from './SignUp';
